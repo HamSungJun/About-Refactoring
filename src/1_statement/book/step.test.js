b34baec113277cbfb20a30e41504377d84b45561
HamSungJun <tjdwns5123@gmail.com>
2021-11-27 17:40:35 +0900

    테스트 소스 주석 추가

--- src/1_statement/book/step.test.js
+++ src/1_statement/book/step.test.js
@@ -5,6 +5,11 @@ import { statement as step3Statement } from './step3'
 import { statement as step4Statement } from './step4'
 import { plays, invoice } from '../variables'
 
+/**
+ * 사소한 수정후 지속적으로 테스트 결과를 확인하면서 레거시 소스코드에서 만족했던 기존의 요구사항을
+ * 리팩토링 과정에서 누락되는 부분은 없는지, 기존의 실행결과와 다르지 않은지 기계적으로 검사하여 시간을 절약한다.
+ */
+
 test('기존 statement 함수의 실행결과와 리팩토링 step1Statement 함수의 실행결과는 동일하다.', () => {
   expect(step1Statement(plays, invoice)).toBe(legacyStatement(plays, invoice))
 })
